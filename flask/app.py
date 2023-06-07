16c68cf053e20d3e0dcf7e0c52d9ed4ebb6035fc
bugavlad96 <47821337+bugavlad96@users.noreply.github.com>
2023-06-07 22:33:03 +0300

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -15,27 +15,33 @@ def validate_email(email):
 
 
 @app.route('/')
-def home():
-    return render_template('login.html')
+def index(logged_in=False):
+    return render_template('main.html', logged_in=logged_in)
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE username = %s", (username,))
-    user = cur.fetchone()
-    cur.close()
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cur.fetchone()
+        cur.close()
 
-    if user and user['password'] == password:
-        session['username'] = username
-        session['name'] = user['name']
-        return redirect('/main')
+        if user and user[5] == password:
+            session['username'] = username
+            session['name'] = user[1]
+            session['surname'] = user[2]
+            session['mail'] = user[3]
+            session['type'] = user[6]
+            return render_template('main.html', logged_in=True, user_name=session['name'], user_type=session['type'])
+        else:
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error, logged_in=False)
     else:
-        error = 'Invalid username or password'
-        return render_template('login.html', error=error)
+        return render_template('login.html')
 
 
 @app.route('/main')
@@ -43,12 +49,13 @@ def main():
     if 'username' in session:
         username = session['username']
         name = session['name']
-        return render_template('main.html', name=name)
+        user_type = session['type']
+        return render_template('main.html', logged_in=True, user_name=name, user_type=user_type)  # Pass the user's type to the template
     else:
         return redirect('/')
 
 
-@app.route('/logout', methods=['POST'])
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.clear()
     return redirect('/')
@@ -95,6 +102,31 @@ def signup():
         return redirect('/')
     else:
         return render_template('signup.html')
+
+
+@app.route('/about')
+def about():
+    user_type = session['type']
+    return render_template('about.html', logged_in=True, user_type=user_type)
+
+
+@app.route('/profile')
+def profile():
+    name = session['name']
+    surname = session['surname']
+    user_type = session['type']
+    mail = session['mail']
+
+    if user_type == "therapist":
+        photo = "doctor.jpg"
+        # photo = "user.jpg"
+        type = "Fizioterapeut"
+
+    else:
+        user_type = "Pacient"
+        photo = "user.jpg"
+
+    return render_template('profile.html', logged_in=True, user_name=name, photo=photo, mail=mail, name=name, surname=surname, user_type=user_type, type=type)
 
 
 if __name__ == '__main__':
